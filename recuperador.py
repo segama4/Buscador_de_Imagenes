5d856edc737ec2d2993f8cfb71da783afa0f7055
triguii <gioserby@gmail.com>
2021-06-09 17:55:40 +0200

    Recuperador v1
    Le pasas un documento y el train, con calcular distancia calcula la distancia con todos los ficheros de la carpeta, y los otros metodos para ir mirando de 5 en 5

---
+++ recuperador.py
@@ -0,0 +1,40 @@
+import os
+import re
+import matplotlib.image as image
+import matplotlib.pyplot as plt
+
+
+class Recuperador ():
+    def __init__(self, document, train):
+        self._document = document
+        self._train = train
+        self._distancies = []
+        self._offset = 0
+
+    def calcula_distancies (self):
+        for fitxer in os.listdir(self._train):
+            self._distancies.append(fitxer.calcula_distancia (self._document))
+
+        self._distancies.sort()
+
+    def mostrar_documents (self):
+        if len(self._distancies) == 0:
+            return None
+        else:
+            return self._distancies[self._offset*5:(self._offset*5)+4]
+
+    def avançar_5 (self):
+        self._offset += 1
+
+    def retornar_5 (self):
+        if self._offset != 0:
+            self._offset -= 1
+        else:
+            raise IndexError
+        
+
+
+
+    
+        
+        
