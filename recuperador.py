a1c85379af36d30cde33b828805314c05ba326e4
triguii <gioserby@gmail.com>
2021-06-12 12:16:43 +0200

    Arreglado un par de cosillas para que cuadre más, pero falta ver lo de index para comprobarlo

--- recuperador.py
+++ recuperador.py
@@ -18,23 +18,27 @@ from vocabulari import Img_Vocabulary
 class Recuperador ():
     def __init__(self, document, index, t_distancia):
         self._index = index
-        self._document = [document for document in [conjunt in  if document.file_name == doc][0]
+        self._arxius_analitzar = []
+        for i in self._index:
+            self._arxius_analitzar.append(self._index[i])
+            if self._index[i].file_name == document:
+                self._document = self._index[i]
         self._distancies = []
-        self._self._arxius_analitzar = []
-        self._
+        
+        
         
         
         if t_distancia == "cosinus": 
-            self._operador = Cosinus(self._train, self._document)
+            self._operador = Cosinus()
         else: 
-            self._operador = Intersection(self._train, self._document)
+            self._operador = Intersection()
 
     def processa_recuperacio(self):
         for fitxer in self._arxius_analitzar:
             if fitxer.file_name != self._document.file_name:
                 self._distancies.append((fitxer, self._operador.calcula_distancia(self._document, fitxer)))
         self._distancies = sorted(self._distancies, key=lambda x: x[1])
-    
+        #No se puede poner self._distancies.sort() y hace lo mismo?
     def get_results(self):
         return [x[0] for x in self._distancies]
 
